44f2f5f13ac5aafdf058c4962dc2b587f565fc0b
k0gang <152839409+k0gang@users.noreply.github.com>
2024-03-24 17:38:14 +0900

    fileManager

--- shop/src/shop/FileManager.java
+++ shop/src/shop/FileManager.java
@@ -1,5 +1,10 @@
 package shop;
 
 public class FileManager {
-
+	private FileWriter fileWriter;
+	private FileReader fileReader;
+	private BufferedReader bufferedReader;
+	private File file;
+	private Stirng fileName;
+	
 }
